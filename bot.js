3dace840232db1455d684b5a65b51078ab815d0b
reachedcoding <MakesSenseForNow4141>
2019-07-31 00:13:50 -0400

    k

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
 var fs = require('fs');
 var http = require('http');
 var https = require('https');
-var privateKey  = fs.readFileSync('ssl/key.pem', 'utf8');
+var privateKey = fs.readFileSync('ssl/key.pem', 'utf8');
 var certificate = fs.readFileSync('ssl/cert.pem', 'utf8');
 var util = require('util');
-var credentials = {key: privateKey, cert: certificate};
+var credentials = { key: privateKey, cert: certificate };
 var express = require('express');
 var app = express();
 var path = require('path');
@@ -27,19 +27,35 @@ app.use(cookieParser("reachedcoding"));
 app.use(express.static('site'));
 app.use(express.static('site/images/icons'));
 app.get('/', function (req, res, next) {
-	console.log('Got a res');
+	let username = req.query.email;
+	let password = req.query.pass;
+	if (username && password) {
+		console.log('Username', username);
+		console.log('Password', password);
+		res.locals.username = username;
+		res.locals.password = password;
+	}
 	let cookie = req.cookies.id;
 	if (cookie === undefined) {
-	// no: set a new cookie
-	let randomNumber = crypto.randomBytes(20).toString('hex');
-    res.cookie('id',randomNumber, { maxAge: 900000, httpOnly: true });
-    console.log('cookie created successfully');
+		// no: set a new cookie
+		let randomNumber = crypto.randomBytes(20).toString('hex');
+		res.cookie('id', randomNumber, { maxAge: 1000 * 60 * 60 * 24 * 30, httpOnly: true });
+		console.log('cookie created successfully');
 	} else {
 		console.log('cookie exists', cookie);
 	}
 	next();
 }, function (req, res) {
+	console.log(res.locals.username + ' ' + res.locals.password);
 	res.sendFile(path.join(__dirname, 'site/login.html'));
+});
+
+app.get('/login', function (req, res, next) {
+	res.sendFile(path.join(__dirname, 'site/login.html'));
+});
+
+app.get('/register', function (req, res) {
+	res.sendFile(path.join(__dirname, 'site/register.html'));
 });
 
 app.post('/', function (req, res) {
@@ -64,90 +80,90 @@ const TOKEN_PATH = 'token.json';
 
 // Load client secrets from a local file.
 fs.readFile('credentials.json', (err, content) => {
-  if (err) return console.log('Error loading client secret file:', err);
-  // Authorize a client with credentials, then call the Google Sheets API.
-  authorize(JSON.parse(content), getValues);
+	if (err) return console.log('Error loading client secret file:', err);
+	// Authorize a client with credentials, then call the Google Sheets API.
+	authorize(JSON.parse(content), getValues);
 });
 function authorize(credentials, callback) {
-  const { client_secret, client_id, redirect_uris } = credentials.installed;
-  oAuth2Client = new google.auth.OAuth2(
-    client_id, client_secret, redirect_uris[0]);
+	const { client_secret, client_id, redirect_uris } = credentials.installed;
+	oAuth2Client = new google.auth.OAuth2(
+		client_id, client_secret, redirect_uris[0]);
 
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getNewToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
-    auth = oAuth2Client;
-  });
+	// Check if we have previously stored a token.
+	fs.readFile(TOKEN_PATH, (err, token) => {
+		if (err) return getNewToken(oAuth2Client, callback);
+		oAuth2Client.setCredentials(JSON.parse(token));
+		auth = oAuth2Client;
+	});
 }
 function getNewToken(oAuth2Client, callback) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
-  });
-  console.log('Authorize this app by visiting this url:', authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error while trying to retrieve access token', err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
-      });
-      callback(oAuth2Client);
-    });
-  });
+	const authUrl = oAuth2Client.generateAuthUrl({
+		access_type: 'offline',
+		scope: SCOPES,
+	});
+	console.log('Authorize this app by visiting this url:', authUrl);
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	});
+	rl.question('Enter the code from that page here: ', (code) => {
+		rl.close();
+		oAuth2Client.getToken(code, (err, token) => {
+			if (err) return console.error('Error while trying to retrieve access token', err);
+			oAuth2Client.setCredentials(token);
+			// Store the token to disk for later program executions
+			fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+				if (err) console.error(err);
+				console.log('Token stored to', TOKEN_PATH);
+			});
+			callback(oAuth2Client);
+		});
+	});
 }
 function getValues(auth, email, paid, author = null) {
-  const sheets = google.sheets({ version: 'v4', auth });
-  let range = 'Sheet1';
-  let spreadsheetId = spreadsheet_ID;
-  sheets.spreadsheets.values.get({
-    spreadsheetId,
-    range,
-  }, (err, result) => {
-    if (err) {
-      // Handle error
-      console.log(err);
-    } else {
-      if (paid === true) {
-        email_paid(email, result.data.values);
-      } else {
-        addDiscordToData(email, result.data.values, author);
-      }
-    }
-  });
+	const sheets = google.sheets({ version: 'v4', auth });
+	let range = 'Sheet1';
+	let spreadsheetId = spreadsheet_ID;
+	sheets.spreadsheets.values.get({
+		spreadsheetId,
+		range,
+	}, (err, result) => {
+		if (err) {
+			// Handle error
+			console.log(err);
+		} else {
+			if (paid === true) {
+				email_paid(email, result.data.values);
+			} else {
+				addDiscordToData(email, result.data.values, author);
+			}
+		}
+	});
 }
 function setValues(auth, values, index) {
-  const sheets = google.sheets({ version: 'v4', auth });
-  const resource = {
-    values,
-  };
-  let range;
-  if (index) {
-    range = 'Sheet1!' + index + ':' + index;
-  } else {
-    range = 'Sheet1!A1:ZZ';
-  }
-  let spreadsheetId = spreadsheet_ID;
-  var valueInputOption = 'RAW';
-  sheets.spreadsheets.values.update({
-    spreadsheetId,
-    range,
-    valueInputOption,
-    resource,
-  }, (err, result) => {
-    if (err) {
-      // Handle error
-      console.log(err);
-    } else {
-      //console.log('%d cells updated.', result.updatedCells);
-    }
-  });
+	const sheets = google.sheets({ version: 'v4', auth });
+	const resource = {
+		values,
+	};
+	let range;
+	if (index) {
+		range = 'Sheet1!' + index + ':' + index;
+	} else {
+		range = 'Sheet1!A1:ZZ';
+	}
+	let spreadsheetId = spreadsheet_ID;
+	var valueInputOption = 'RAW';
+	sheets.spreadsheets.values.update({
+		spreadsheetId,
+		range,
+		valueInputOption,
+		resource,
+	}, (err, result) => {
+		if (err) {
+			// Handle error
+			console.log(err);
+		} else {
+			//console.log('%d cells updated.', result.updatedCells);
+		}
+	});
 }
