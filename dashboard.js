357f17c3705abc1d88d8b62e6c0ed646e253ef4f
Shivam Singh <reachedcoding@gmail.com>
2019-11-10 01:37:38 -0500

    ok

--- dashboard.js
+++ dashboard.js
@@ -16,7 +16,6 @@ const request = require('request');
 const fetch = require('node-fetch');
 const FormData = require('form-data');
 const rp = require('request-promise');
-
 let spreadsheet_ID, db, users, client_id, client_secret;
 
 fs.readFile('settings.json', (err, content) => {
@@ -42,81 +41,95 @@ app.use(express.static('site/dashboard/'));
 app.use(express.static('site/images/icons'));
 app.set('view engine', 'ejs');
 
-app.use('/', function (req, res, next) {
+const login_url = encodeURI("https://localhost/login");
+app.use('/*', function (req, res, next) {
 	if (req.method === 'GET' || req.method === 'HEAD') {
-		if (req.cookies.id) {
-
+		if (req.cookies.a && req.cookies.r) {
 			next();
 		} else {
-			let randomNumber = crypto.randomBytes(20).toString('hex');
-			res.cookie('id', randomNumber, { maxAge: 1000 * 60 * 60 * 24 * 30, httpOnly: true });
-			console.log('cookie created successfully', randomNumber);
-			res.redirect('https://discordapp.com/api/oauth2/authorize?response_type=code&client_id=608328061699620865&scope=identify%20email%20guilds&redirect_uri=https%3A%2F%2Flocalhost&prompt=consent');
+			let code = req.query.code;
+			if (code)
+				next();
+			else
+				res.redirect(`https://discordapp.com/api/oauth2/authorize?response_type=code&client_id=608328061699620865&scope=identify%20email%20guilds&redirect_uri=${login_url}&prompt=consent`);
 		}
 	} else
 		next();
 });
 
 app.get('/', async function (req, res, next) {
-	let code = req.query.code;
-	if (code) {
-		const data = new FormData();
-
-		data.append('client_id', client_id);
-		data.append('client_secret', client_secret);
-		data.append('grant_type', 'authorization_code');
-		data.append('redirect_uri', 'https://localhost');
-		data.append('scope', 'identify email guilds');
-		data.append('code', code);
-
-		try {
-		let response = await rp.post('https://discordapp.com/api/oauth2/token', {
-			form: {
-				client_id: client_id,
-				client_secret: client_secret,
-				grant_type: 'authorization_code',
-				redirect_uri: 'https://localhost',
-				scope: 'identify email guilds',
-				code: code
-			},
-		});
-		let info = JSON.parse(response);
+	try {
+		let a = req.cookies.a;
+		let access_token = decrypt(a);
 		let response2 = await rp.get('https://discordapp.com/api/users/@me', {
 			headers: {
-				authorization: `${info.token_type} ${info.access_token}`,
+				authorization: `Bearer ${access_token}`,
 			}
-		});
+		}).catch(e => { });
 		let discordUser = JSON.parse(response2);
-		let id = discordUser.id;
-		let values = await getValues();
-		let found;
-		for (i in values) {
-			if (values[i][0] == id) {
-				found = i;
-			}
-		}
-		values.push([id]);
-		if (!found) {
-			setValues(auth, values, found + 1);
-		}
-		if (discordUser.code && discordUser.code == 0) {
-			res.locals.site = "Unauthorized";
-		} else {
-			res.locals.site = discordUser;
-		}
-	} catch {
-		res.locals.site = "Unauthorized";
+		res.locals.site = discordUser;
+		next();
+	} catch (e) {
+		res.redirect(`https://discordapp.com/api/oauth2/authorize?response_type=code&client_id=608328061699620865&scope=identify%20email%20guilds&redirect_uri=${login_url}&prompt=consent`);
 	}
-	} else {
-		res.locals.site = false;
-	}
-	next();
 }, function (req, res) {
 	if (res.locals.site) {
 		res.send(res.locals.site);
 	} else {
 		res.render(path.join(__dirname, 'site/dashboard/pages/index.ejs'));
 	}
+});
+
+app.get('/login', async function (req, res, next) {
+	let code = req.query.code;
+	if (code) {
+		try {
+			let response = await rp.post('https://discordapp.com/api/oauth2/token', {
+				form: {
+					client_id: client_id,
+					client_secret: client_secret,
+					grant_type: 'authorization_code',
+					redirect_uri: login_url,
+					scope: 'identify email guilds',
+					code: code
+				},
+			});
+			let info = JSON.parse(response);
+			var a = encrypt(info.access_token);
+			var r = encrypt(info.refresh_token);
+			res.cookie('a', a, { maxAge: 1000 * 60 * 60 * 24 * 365, httpOnly: true });
+			res.cookie('r', r, { maxAge: 1000 * 60 * 60 * 24 * 365, httpOnly: true });
+			let id = discordUser.id;
+			let values = await getValues();
+			let found;
+			for (i in values) {
+				if (values[i][0] == id) {
+					found = i;
+				}
+			}
+			values.push([id]);
+			if (!found) {
+				setValues(auth, values, found + 1);
+			}
+			if (discordUser.code && discordUser.code == 0) {
+				res.locals.site = "Unauthorized";
+			} else {
+				res.locals.site = discordUser;
+			}
+		} catch (e) {
+			res.locals.site = "An Error has Occured";
+		}
+	} else {
+		res.locals.site = "Unauthorized";
+	}
+	next();
+}, function (req, res) {
+	res.redirect('/');
+	// if (res.locals.site) {
+	// 	res.send(res.locals.site);
+	// } else {
+	// 	res.render(path.join(__dirname, 'site/dashboard/pages/index.ejs'));
+	// }
 });
 
 // app.get('/', function (req, res, next) {
@@ -154,6 +167,23 @@ app.get('/', async function (req, res, next) {
 // });
 
 // EXPRESS HELPER FUNCTIONS
+
+var algorithm = 'aes-256-ctr',
+	password = 'd6F3Efeq';
+
+function encrypt(text) {
+	var cipher = crypto.createCipher(algorithm, password)
+	var crypted = cipher.update(text, 'utf8', 'hex')
+	crypted += cipher.final('hex');
+	return crypted;
+}
+
+function decrypt(text) {
+	var decipher = crypto.createDecipher(algorithm, password)
+	var dec = decipher.update(text, 'hex', 'utf8')
+	dec += decipher.final('utf8');
+	return dec;
+}
 
 function checkCookies(req, res) {
 	let cookie = req.cookies.id;
@@ -314,5 +344,5 @@ class User {
 }
 
 async function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+	return new Promise(resolve => setTimeout(resolve, ms));
 }
