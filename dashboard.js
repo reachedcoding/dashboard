eedc1e7328ecec7656bd935a2c11b945a941a8eb
Shivam Singh <reachedcoding@gmail.com>
2019-11-15 00:08:48 -0500

    COMMENTS

--- dashboard.js
+++ dashboard.js
@@ -1,3 +1,4 @@
+// IMPORT LIBRARIES
 var fs = require('fs');
 var http = require('http');
 var https = require('https');
@@ -10,19 +11,15 @@ var app = express();
 var path = require('path');
 var cookieParser = require('cookie-parser')
 const crypto = require("crypto");
-const { google } = require('googleapis');
-const readline = require('readline');
-const request = require('request');
-const fetch = require('node-fetch');
-const FormData = require('form-data');
 const rp = require('request-promise');
 const MongoClient = require("mongodb").MongoClient;
-const ObjectId = require("mongodb").ObjectID;
 
-let spreadsheet_ID, db, users, client_id, client_secret;
+// DATABASE INFO
+let db, client_id, client_secret;
 const CONNECTION_URL = 'mongodb+srv://SERVER_ACCESS:4141@reachedio-server-dlyzm.mongodb.net/test?retryWrites=true&w=majority';
 const DATABASE_NAME = "ReachedIO";
 
+// TEMPLATE FOR ADMIN USER OBJECT
 let adminObj = {
 	id: "",
 	next_payment: "",
@@ -30,6 +27,7 @@ let adminObj = {
 	first_login: ""
 };
 
+// TEMPLATE FOR REGULAR USER OBJECT
 let userObj = {
 	id: "",
 	next_payment: "",
@@ -37,6 +35,7 @@ let userObj = {
 	first_login: ""
 };
 
+// LOAD CONFIGURATION
 fs.readFile('settings.json', (err, content) => {
 	if (err) return console.log('Error loading settings:', err);
 	var settings = JSON.parse(content);
@@ -45,12 +44,7 @@ fs.readFile('settings.json', (err, content) => {
 	client_secret = settings.client_secret;
 });
 
-fs.readFile('db.json', (err, content) => {
-	if (err) return console.log('Error loading settings:', err);
-	db = JSON.parse(content);
-	users = db.users;
-});
-
+// WEBSITE ACCESSIBILITY AND FLOW
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser("reachedcoding"));
@@ -60,7 +54,10 @@ app.use(express.static('site/dashboard/'));
 app.use(express.static('site/images/icons'));
 app.set('view engine', 'ejs');
 
+// OAUTH2 FLOW LOGIN CALLBACK
 const login_url = encodeURI("https://dashboard.reachedcoding.com/login");
+
+// MIDDLEWARE ON ROOT DOMAIN -- NOTHING FOR NOW
 app.use('/', function (req, res, next) {
 	if (req.method === 'GET' || req.method === 'HEAD') {
 		if (req.cookies.a && req.cookies.r) {
@@ -76,11 +73,16 @@ app.use('/', function (req, res, next) {
 		next();
 });
 
+// ROOT DOMAIN QUERY
+
 app.get('/', async function (req, res, next) {
 	let discordUser;
 	try {
-		let a = req.cookies.a;
+		// GET COOKIES FROM REQUEST
+		let a = req.cookies.a; 
+		// DECRYPT THE ACCESS TOKEN COOKIE -- IF DOES NOT EXIST OR WRONGLY ENCODED, THROWS ERROR AND GOES TO THE CATCH BLOCK
 		let access_token = decrypt(a);
+		// REQUEST DISCORD_USER OBJECT FROM DISCORD'S API USING THE ACCESS TOKEN -- MAY ALSO THROW ERROR
 		let response2 = await rp.get('https://discordapp.com/api/users/@me', {
 			headers: {
 				authorization: `Bearer ${access_token}`,
@@ -88,13 +90,14 @@ app.get('/', async function (req, res, next) {
 		}).catch(e => { });
 		discordUser = JSON.parse(response2);
 		console.log(`${discordUser.username}#${discordUser.discriminator} logged in!`);
-		res.locals.site = discordUser;
+		// ALLOWS PASSING OF THE DISCORD USER_OBJECT BETWEEN METHODS
 		let values = [];
 		for (var key in discordUser) {
 			if (discordUser.hasOwnProperty(key)) {
 				values.push({ "key": key, "value": discordUser[key] })
 			}
 		}
+		// ALLOWS PASSING OF THE DISCORD USER_OBJECT BETWEEN METHODS
 		res.locals.ejs = values;
 		res.locals.site = discordUser;
 		next();
@@ -103,6 +106,7 @@ app.get('/', async function (req, res, next) {
 	}
 
 }, function (req, res) {
+	// CHECKS WHETHER DATA HAS BEEN RECEIVED AND SHOWS IT OR SHOWS THE MAIN LOGIN SCREEN
 	if (!res.locals.ejs) {
 		res.send(res.locals.site);
 	} else {
@@ -113,11 +117,15 @@ app.get('/', async function (req, res, next) {
 	}
 });
 
+// LOGIN DOMAIN QUERY
 app.get('/login', async function (req, res, next) {
+	// GET CODE FROM URL_ENCODED_VALUES - SHOULD BE A CALLBACK FROM DISCORD'S OAUTH2 FLOW
 	let code = req.query.code;
 	if (code) {
 		try {
 			res.locals.code = true;
+
+			// AQUIRE ACCESS TOKEN AND REFRESH TOKEN FROM DISCORD'S API
 			let response = await rp.post('https://discordapp.com/api/oauth2/token', {
 				form: {
 					client_id: client_id,
@@ -129,22 +137,17 @@ app.get('/login', async function (req, res, next) {
 				},
 			});
 			let info = JSON.parse(response);
+
+			// ENCRYPT ACCESS AND REFRESH TOKENS
 			var a = encrypt(info.access_token);
 			var r = encrypt(info.refresh_token);
+
+			// SET COOKIE'S TO THE ENCRYPTED ACCESS AND REFRESH TOKENS
 			res.cookie('a', a, { maxAge: 1000 * 60 * 60 * 24 * 365, httpOnly: true });
 			res.cookie('r', r, { maxAge: 1000 * 60 * 60 * 24 * 365, httpOnly: true });
 			let id = discordUser.id;
-			let values = await getValues();
-			let found;
-			for (i in values) {
-				if (values[i][0] == id) {
-					found = i;
-				}
-			}
-			values.push([id]);
-			if (!found) {
-				setValues(auth, values, found + 1);
-			}
+			
+			// SPECIAL RESPONSE FROM DISCORD'S API SAYING NOT AUTHORIZED
 			if (discordUser.code && discordUser.code == 0) {
 				res.locals.site = "Unauthorized";
 			} else {
@@ -158,17 +161,14 @@ app.get('/login', async function (req, res, next) {
 	}
 	next();
 }, function (req, res) {
+	// IF RECEIVED CODE AS SUCCESSFULLY PARSED THEN PASS TO ROOT DOMAIN DIRECTORY ELSE TRY AUTHENTICATING AGIAN
 	if (res.locals.code)
 		res.redirect('/')
 	else
 		res.redirect(`https://discordapp.com/api/oauth2/authorize?response_type=code&client_id=608328061699620865&scope=identify%20email%20guilds&redirect_uri=${login_url}&prompt=consent`);
-	// if (res.locals.site) {
-	// 	res.send(res.locals.site);
-	// } else {
-	// 	res.render(path.join(__dirname, 'site/dashboard/pages/index.ejs'));
-	// }
 });
 
+// LOGOUT DOMAIN QUERY -- DELETES ACCESS AND REFRESH COOKIES THEN REDIRECTS TO ROOT DOMAIN
 app.get('/logout', async function (req, res, next) {
 	res.clearCookie("a");
 	res.clearCookie("r");
@@ -177,18 +177,24 @@ app.get('/logout', async function (req, res, next) {
 	res.redirect('/');
 });
 
+// NOT REALLY IMPLEMENTED YET BUT MAY SERVE AS A HOMEPAGE
 app.get('/home', function (req, res) {
 	res.render(path.join(__dirname, 'site/dashboard/pages/home.ejs'));
 });
 
+// TEST DOMAIN QUERY -- USED TO TEST MONGODB DATABASE FUNCTIONALITY AND INTEGRATION
 app.get('/test', async function (req, res) {
 	try {
+		// GET DISCORD ID FROM COOKIES AND DISCORD API REQUEST
 		let id = await getDiscordId(req);
 		if (id)
+			// FIND AN ID IN THE DATABASE WITH THE SAME ID AND RETURN ALL DATABASE DATA FOR THAT KEY
+			// IF NOT FOUND, ADD TO THE DATABASE AND REFRESH
 			collection.find({ "id": id }).toArray((error, result) => {
 				if (error) {
 					return res.status(500).send(error);
 				}
+				// IF ID NOT FOUND
 				if (result.length == 0) {
 					userObj.id = id;
 					userObj.first_login = new Date();
@@ -218,7 +224,7 @@ app.get('/test', async function (req, res) {
 	}
 });
 
-// EXPRESS HELPER FUNCTIONS
+// ENCRYTION/DECRYPTION LOGIC
 
 var algorithm = 'aes-256-ctr',
 	password = 'd6F3Efeq';
@@ -237,6 +243,7 @@ function decrypt(text) {
 	return dec;
 }
 
+// FROM REQUEST RETURNS THE ID OF THE USER WITH THAT ACCESS TOKEN
 async function getDiscordId(req) {
 	try {
 		let a = req.cookies.a;
@@ -256,10 +263,13 @@ async function getDiscordId(req) {
 // EXPRESS SERVER
 var database, collection;
 
+// CREATES SERVER FROM APP LOGIC -- BOTH HTTP AND HTTPS
 var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
 
+// LISTEN ON HTTP AND HTTPS PORTS AND SERVE
 httpServer.listen(80, () => {
+	// MONGODB INTEGRATION - ESTABLISHES CONNECTION WITH THE DATABASE
 	MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
 		if (error) {
 			throw error;
@@ -271,6 +281,7 @@ httpServer.listen(80, () => {
 });
 httpsServer.listen(443);
 
+// HELPFUL FUNCTION IF EVER NEED TO SLEEP
 async function sleep(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
