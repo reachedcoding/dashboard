34f1ec342042a0facd4749494cb9d4946e7e0f48
Sagar Singh <sagarsingh50@gmail.com>
2019-11-18 20:02:09 -0500

    test charges to stripe now work. Created functions for future stripe usage

--- stripe.js
+++ stripe.js
@@ -6,7 +6,7 @@ const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
 const stripePublicKey = process.env.STRIPE_PUBLIC_KEY;
 
 const stripe = require("stripe")("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
-
+const uuidv4 = require('uuid/v4');
 var price = price * 100;
 
 
@@ -16,41 +16,42 @@ function renew(id) {
   })
 }
 
-stripe.charges.create({
-  amount: 2000,
-  currency: "usd",
-  source: "tok_visa", // obtained with Stripe.js
-  description: "Charge for jenny.rosen@example.com"
-}, {
-  idempotency_key: "FWTSYxcz76PVzlPZ"
-}, function(err, charge) {
-      switch (err) {
-      case 'StripeCardError':
-        // A declined card error
-        err.message; // => e.g. "Your card's expiration year is invalid."
-        break;
-      case 'StripeRateLimitError':
-        // Too many requests made to the API too quickly
-        break;
-      case 'StripeInvalidRequestError':
-        // Invalid parameters were supplied to Stripe's API
-        break;
-      case 'StripeAPIError':
-        // An error occurred internally with Stripe's API
-        break;
-      case 'StripeConnectionError':
-        // Some kind of error occurred during the HTTPS communication
-        break;
-      case 'StripeAuthenticationError':
-        // You probably used an incorrect API key
-        break;
-      default:
-        // Handle any other types of unexpected errors
-        break;
-      }
-      console.log(charge);
-});
-
+async function single_charge(amount, currency, source, description){
+  await stripe.charges.create({
+    amount: 2000,
+    currency: "usd",
+    source: "tok_visa", // obtained with Stripe.js
+    description: "Charge for jenny.rosen@example.com"
+  }, {
+    idempotency_key: uuidv4()
+  }, function(err, charge) {
+        switch (err) {
+        case 'StripeCardError':
+          // A declined card error
+          err.message; // => e.g. "Your card's expiration year is invalid."
+          break;
+        case 'StripeRateLimitError':
+          // Too many requests made to the API too quickly
+          break;
+        case 'StripeInvalidRequestError':
+          // Invalid parameters were supplied to Stripe's API
+          break;
+        case 'StripeAPIError':
+          // An error occurred internally with Stripe's API
+          break;
+        case 'StripeConnectionError':
+          // Some kind of error occurred during the HTTPS communication
+          break;
+        case 'StripeAuthenticationError':
+          // You probably used an incorrect API key
+          break;
+        default:
+          // Handle any other types of unexpected errors
+          break;
+        }
+        console.log(charge);
+  });
+}
 
 
 function subscribe(plan_, customer_) {
@@ -89,6 +90,7 @@ function subscribe(plan_, customer_) {
   );
 }
 
+single_charge();
 
 function cancel_subscription(id) {
   stripe.subscriptions.del(
@@ -97,4 +99,3 @@ function cancel_subscription(id) {
 
     });
 }
-subscribe();
